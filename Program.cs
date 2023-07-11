a2e206a3bc2505a4c38fa3ab425e30d0433142f9
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-07-11 17:07:45 +0300

    Done task 6

--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Console.WriteLine("Enter your text :");
+            string text = Console.ReadLine();
+            string[] sentences = text.Split('.');
+            for (int i = 0; i < sentences.Length; i++)
+            {
+                string sentence = sentences[i].Trim();
+                if (sentence.Length > 0)
+                {
+                    string firstLetter = sentence.Substring(0, 1);
+                    string rest = sentence.Substring(1);
+                    string result = firstLetter.ToUpper() + rest;
+                    sentences[i] = result;
+                }
+            }
+            string resultText = string.Join(". ", sentences);
+            Console.WriteLine(resultText);
         }
     }
 }
