248f637eec9c53f8c6ce3ffb393f311895553fb0
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-07-11 17:21:13 +0300

    Done task5

--- Program.cs
+++ Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Console.WriteLine("Enter the expression");
+            string expression = Console.ReadLine();
+            int result = 0;
+            int number = 0;
+            char sign = '+';
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '+' || expression[i] == '-')
+                {
+                    sign = expression[i];
+                }
+                else
+                {
+                    number = int.Parse(expression[i].ToString());
+                    if (sign == '+')
+                        result += number;
+                    else
+                        result -= number;
+                }
+            }
+            Console.WriteLine($"Result: {result}");
         }
     }
 }
