4be427a5249240c60dc4583551fbaa0009945e9a
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-07-11 17:04:50 +0300

    Done task2

--- Program.cs
+++ Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            int[,] array = new int[5, 5];
+            Random random = new Random();
+            int min = 0;
+            int max = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    array[i, j] = random.Next(-100, 100);
+                    Console.Write(array[i, j] + "\t");
+                    if (array[i, j] < min)
+                    {
+                        min = array[i, j];
+                    }
+                    if (array[i, j] > max)
+                    {
+                        max = array[i, j];
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Min value : {min}");
+            Console.WriteLine($"max value : {max}");
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                bool isMin = false;
+                bool isMax = false;
+                for (int j = 0; j < 5; j++)
+                {
+                    if (array[i, j] == min)
+                    {
+                        isMin = true;
+                    }
+                    if (array[i, j] == max)
+                    {
+                        isMax = true;
+                    }
+                    if (isMin && !isMax || isMax && !isMin)
+                    {
+                        sum += array[i, j];
+                    }
+                }
+            }
+            Console.WriteLine($"Result {sum}");
         }
     }
 }
