bd214167913dda81cbbc8d4936c3c44df87f2e79
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-07-11 21:23:58 +0300

    Done task4

--- Program.cs
+++ Program.cs
@@ -2,44 +2,67 @@
 {
     internal class Program
     {
+        static void PrintMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+            for (int i = 0; i < rows; i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{matrix[i, j],3}");
+                }
+                Console.WriteLine("|");
+            }
+            Console.WriteLine();
+        }
+        static int[,] MultiplyMatrixOnNumber(int[,] matrix, int number)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+            int[,] result = new int[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++) result[i, j] = matrix[i, j] * number;
+            }
+            return result;
+        }
+        static int[,] AddMatrix(int[,] matrix1, int[,] matrix2)
+        {
+            int rows = matrix1.GetUpperBound(0) + 1;
+            int columns = matrix1.Length / rows;
+            int[,] result = new int[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++) result[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+            return result;
+        }
+        static int[,] MultiplyMatrix(int[,] matrix1, int[,] matrix2)
+        {
+            int rows = matrix1.GetUpperBound(0) + 1;
+            int columns = matrix1.Length / rows;
+            int[,] result = new int[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++) result[i, j] = matrix1[i, j] * matrix2[i, j];
+            }
+            return result;
+        }
         static void Main(string[] args)
         {
-            /*Створіть додаток, який здійснює операції над матрицями:
-■ Множення матриці на число;
-■ Додавання матриць;
-■ Добуток матриць*/
-            int[,] array = new int[5, 5];
-            Random random = new Random();
-            //додавання матриць
-            for (int i = 0; i < 5; i++)
-            {
-                int sum = 0;
-                for (int j = 0; j < 5; j++)
-                {
-                    array[i, j] = random.Next(0, 10);
-                    Console.Write($"{array[i, j]} ");
-                    sum += array[i, j];
-                }
-                Console.WriteLine($"sum : {sum}");
-            }
-            // множення матриці на число
-            int number = 5;
-            Console.WriteLine($"number : {number}");
-            for (int i = 0; i < 5; i++)
-            {
-                int sum = 0;
-                for (int j = 0; j < 5; j++)
-                {
-                    array[i, j] *= number;
-                    Console.Write($"{array[i, j]} ");
-                    sum += array[i, j];
-                }
-                Console.WriteLine($"sum : {sum}");
-            }
-            // добуток матриць
-
-            }
-
+            int[,] AB = new int[3, 3] { {1, 2, 3},{4, 5, 6},{7, 8, 9} };
+            PrintMatrix(AB);
+            //multiply matrix on number
+            AB = MultiplyMatrixOnNumber(AB, 5);
+            PrintMatrix(AB);
+            //add matrix
+            AB = AddMatrix(AB, AB);
+            PrintMatrix(AB);
+            //multiply matrix
+            AB = MultiplyMatrix(AB, AB);
+            PrintMatrix(AB);
         }
     }
 }
