63919835bd7517be037b04feb901f08d1f4f5535
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-07-11 17:24:40 +0300

    4

--- Program.cs
+++ Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            /*Створіть додаток, який здійснює операції над матрицями:
+■ Множення матриці на число;
+■ Додавання матриць;
+■ Добуток матриць*/
+            int[,] array = new int[5, 5];
+            Random random = new Random();
+            //додавання матриць
+            for (int i = 0; i < 5; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < 5; j++)
+                {
+                    array[i, j] = random.Next(0, 10);
+                    Console.Write($"{array[i, j]} ");
+                    sum += array[i, j];
+                }
+                Console.WriteLine($"sum : {sum}");
+            }
+            // множення матриці на число
+            int number = 5;
+            Console.WriteLine($"number : {number}");
+            for (int i = 0; i < 5; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < 5; j++)
+                {
+                    array[i, j] *= number;
+                    Console.Write($"{array[i, j]} ");
+                    sum += array[i, j];
+                }
+                Console.WriteLine($"sum : {sum}");
+            }
+            // добуток матриць
+
+            }
+
         }
     }
 }
